74ad1e3d77362cd31c2a67a17acef72ab4bb3715
thalvari <tuomas.halvari@helsinki.fi>
2019-01-28 14:09:42 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = ({text}) => <h3>{text}</h3>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const Display = ({text, value}) => <div>{text} {value}</div>
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGood = () => setGood(good + 1)
+    const handleNeutral = () => setNeutral(neutral + 1)
+    const handleBad = () => setBad(bad + 1)
+
+    return (
+        <div>
+            <Header text={'anna palautetta'}/>
+            <Button handleClick={handleGood} text="hyvä"/>
+            <Button handleClick={handleNeutral} text="neutraali"/>
+            <Button handleClick={handleBad} text="huono"/>
+            <Header text={'statistiikka'}/>
+            <Display text={'hyvä'} value={good}/>
+            <Display text={'neutraali'} value={neutral}/>
+            <Display text={'huono'} value={bad}/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
